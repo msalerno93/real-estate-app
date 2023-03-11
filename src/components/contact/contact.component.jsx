3e0bf91c5d3c067a34fab6957ed91b3a76c18870
Michael Salerno <msalerno1993@gmail.com>
2023-03-10 22:47:01 -0500

    started contact us form - styling almost completed, most functionality completed

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,1 +1,50 @@
 // contact form maybe a little header
+import './contact.styles.scss'
+
+
+import ADDRESS from '../../houseinfo.json'
+import AGENT from '../../realtors.json'
+
+const ContactUsCard = () => {
+  return (
+    <div>
+      <form className='contact-us-form-container' method='post' action="msalerno1993@gmail.com">
+        <label className='label'>
+          Name:
+          <input className='text-input' type="text" name="name" />
+        </label>
+        <label className='label'>
+          Email Address:
+          <input className='text-input' type="email" name="name" />
+        </label>
+        <label className='label'>
+          Phone Number:
+          <input className='text-input' type="text" name="name" />
+        </label>
+        <label className='label'>
+          Listing Address:
+          <select className='text-input' type="text" name="name">
+            {ADDRESS.map((add) => {
+                return (
+                <option key={add.id} value={add.id}>{add.address}</option>
+                )
+            })}
+          </select>
+        </label>
+        <label className='label'>
+          Listing Agent:
+          <select className='text-input'  type="text" name="name">
+            {AGENT.map((agent) => {
+                return (
+                <option key={agent.id} value={agent.id}>{agent.name}</option>
+                )
+            })}
+          </select>
+        </label>
+        <button className='submit-button'> Submit </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactUsCard;
--- src/routes/contact-us/contact-us.component.jsx
+++ src/routes/contact-us/contact-us.component.jsx
@@ -1,12 +1,14 @@
 // contact component to be shown here
 //should have form to be submitted to contact
 
-import React from 'react'
+import './contact-us.styles.scss'
+import ContactUsCard from '../../components/contact/contact.component'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs
-    
+    <div>
+      <div className="title-contact">Contact Us!</div>
+      <ContactUsCard />
     </div>
   )
 }
