3f3d0e7cc03a5854e34061c784fb25f0df0aa80e
Michael Salerno <msalerno1993@gmail.com>
2023-03-12 19:43:55 -0400

    completed showpage with back button and stylings

--- src/routes/home-showpage/home-showpage.component.jsx
+++ src/routes/home-showpage/home-showpage.component.jsx
@@ -3,11 +3,16 @@
 //shows only one specific house listing with all information
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 import HouseShowpage from '../../components/house-showpage-card/house-showpage-card.component'
+import './home-showpage.styles.scss'
 
 const HomeShowpage = () => {
   return (
-    <div>
+    <div className='homeshowpage-container'>
+      <div>
+        <Link className='back-button' to="/listings">Back to Listings</Link>
+      </div>
       <HouseShowpage />
     </div>
   )
